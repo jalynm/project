b8c242a597fdeb0d44df2da23aba61702bf7f471
Yellowazns123 <71678250+Yellowazns123@users.noreply.github.com>
2020-11-18 17:06:51 -0800

    Add files via upload

--- aowejLOLALOLA.py
+++ aowejLOLALOLA.py
@@ -1,7 +1,19 @@
 import vlc
 import pafy
 
-url = "https://www.youtube.com/watch?v=erG5rgNYSdk"
+import urllib.request
+import urllib.parse
+import re
+
+import time
+
+search_input = input("Search: ") + " audio"
+query_string = urllib.parse.urlencode({"search_query" : search_input})
+html_content = urllib.request.urlopen("https://www.youtube.com/results?" + query_string)
+video_ids = re.findall(r"watch\?v=(\S{11})", html_content.read().decode())
+url = "https://www.youtube.com/watch?v=" + video_ids[0]
+print(video_ids[0])
+
 video = pafy.new(url)
 best = video.getbest()
 playurl = best.url
@@ -11,4 +23,5 @@ player = Instance.media_player_new()
 Media = Instance.media_new(playurl)
 Media.get_mrl()
 player.set_media(Media)
+player.audio_set_volume(80)
 player.play()
